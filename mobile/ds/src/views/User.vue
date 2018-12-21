705a9cae9690c696871b5ba25940097bc420e528
lsc656 <854332104@qq.com>
2018-12-21 17:00:10 +0800

    cdhbiq

--- mobile/ds/src/views/User.vue
+++ mobile/ds/src/views/User.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="user">
-    User 进入前先判断login状态。若无登录，跳转/Login
+    <div class="bookImg">
+      <ul>
+        <li><img src="http://127.0.0.1:3000/images/Index/index-2m-1.jpg" alt=""></li>
+        <li><img src="http://127.0.0.1:3000/images/Index/index-2m-1.jpg" alt=""></li>
+      </ul>
+    </div>
+    <div>X本已购</div>
+    <div class="book-list">
+      <div>书架为空</div>
+      <ul>
+        <li>书1</li>
+        <li>书2</li>
+        <li>书3</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -39,6 +53,40 @@
   }
 </script>
 <style>
+  div.user{
 
+  }
+  div.user>div.bookImg{
+    height:170px;
+    background:linear-gradient(to top,rgb(37, 131, 150) 0% , #1DABC5 10%);
+  }
+  div.user>div.bookImg>ul{
+    height:100%;
+    list-style: none;
+    display:flex;
+    flex-wrap:nowrap;
+    justify-content: space-around;
+    align-items:flex-end;
+    margin:0;
+    padding:0;
+    overflow:hidden;
+  }
+  div.user>div.bookImg>ul>li{
+    width:25%;
+    box-shadow:4px 5px 3px 4px rgb(37, 131, 150);
+  }
+  div.user>div.bookImg>ul>li>img{
+    width:100%;
+    vertical-align: bottom;
+  }
+  div.user>div:nth-child(2){
+    padding:20px;
+    border-bottom:1px solid #CACACA;
+    font-size:14px;
+    color:#4C4C4C;
+  }
+  div.user>div.book-list{
+    
+  }
 </style>
 
