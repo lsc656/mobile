2e96ca154fb2dbdbc4851604a269c895465eda3e
lsc656 <854332104@qq.com>
2019-01-02 20:51:29 +0800

    dqwio

--- wx_00/wx/pages/myFavorite/myFavorite.js
+++ wx_00/wx/pages/myFavorite/myFavorite.js
@@ -1,20 +1,18 @@
-// pages/pins/pins.js
+// pages/myFavorite/myFavorite.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    pid:0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.setData({
-    //   pid:options.pid
-    // })
+
   },
 
   /**
